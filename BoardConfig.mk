d648f6db8b9f9e9b4e099451b7146c1bf289aba6
dhimanparas20 <dhimanparas20@gmail.com>
2022-01-31 19:06:04 +0530

    sweet: Device build Fix-up * Update call back name of libinit * also update TARGET_KERNEL_SOURCE
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,11 +16,11 @@ TARGET_OTA_ASSERT_DEVICE := sweet,sweetin
 TARGET_PROVIDES_AUDIO_EXTNS := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sweet
-TARGET_RECOVERY_DEVICE_MODULES := libinit_sweet
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_sweet
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_sweet
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
 TARGET_KERNEL_CONFIG := sweet_user_defconfig
 
 # Inherit from proprietary files
