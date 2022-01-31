8831f8423dabdffbfa137d936ae9ec4a8c0615ca
SebaUbuntu <barezzisebastiano@gmail.com>
2022-01-31 08:53:13 +0530

    sweet: libinit: Adapt to latest libinit changes
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,10 @@ TARGET_OTA_ASSERT_DEVICE := sweet,sweetin
 # Audio
 TARGET_PROVIDES_AUDIO_EXTNS := true
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sweet
+TARGET_RECOVERY_DEVICE_MODULES := libinit_sweet
+
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CONFIG := sweet_user_defconfig
